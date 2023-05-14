47e4b6947da8cd961c31c9406a255c265cdf55b1
danniel-contreras <87451036+danniel-contreras@users.noreply.github.com>
2023-05-14 16:21:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.send(req)
+  console.log(req)
+  return res.send("Hola Mundo")
 });
 
 app.listen(2000)
