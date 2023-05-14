c69eb24b04ef5b59369e74b69adb599f57df8b55
danniel-contreras <87451036+danniel-contreras@users.noreply.github.com>
2023-05-14 16:16:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
 const express = require('express');
 const app = express();
-const os = require('os');
-const dns = require('dns');
-
-const getLocalIp = (req, callback) => {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  const localhost = '::1';
-
-  if (ip === localhost) {
-    return callback('127.0.0.1');
-  }
-
-  dns.reverse(ip, (err, hostnames) => {
-    if (err) {
-      return callback(null);
-    }
-
-    dns.lookup(hostnames[0], (err, address) => {
-      if (err) {
-        return callback(null);
-      }
-
-      callback(address);
-    });
-  });
-};
 
 app.get('/', (req, res) => {
-  getLocalIp(req, (localIp) => {
-    res.send(`Tu dirección IP local es: ${localIp}`);
-  });
+  return res.send(req)
 });
 
 app.listen(2000)
